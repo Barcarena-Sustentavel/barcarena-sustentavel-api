935cb3f731d291f3cc5fecc8ac0252e3ff1396f5
Marrior <mrlimaroberto@gmail.com>
2024-11-30 21:45:43 -0300

    endpoints da última api e models segunda novo modelo foram criados

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
+from fastapi import FastAPI
+from app.api.v1.endpoints.dimensoes import router
+
+app = FastAPI()
+app.include_router(router)
+
 def print_hi(name):
     print(f'Hi, {name}')
 
-
 if __name__ == '__main__':
+   
     print_hi('PyCharm')
 
