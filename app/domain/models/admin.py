d220282bfe214fa0f500aa189d6bfc6a6f5ed999
Marrior <mrlimaroberto@gmail.com>
2024-12-09 22:17:29 -0300

    admin.py concertado

--- app/domain/models/admin.py
+++ app/domain/models/admin.py
@@ -7,10 +7,11 @@ class DimensaoContribuicao(Base):
     __table_args__ = {"schema": "barcarena_sustentavel"} #schema
 
     id = Column(Integer, primary_key=True)
-    nome = Column(String, nullable=True)
-    sobrenome = Column(String, nullable=True)
-    email = Column(String, nullable=True)
+    nome = Column(String, nullable=False)
+    sobrenome = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    role = Column(String, nullable=False)
     password_hash = Column(String, nullable=False)
-    created_at = Column(DateTime, nullable=True)
-    update_at = Column(DateTime, nullable=False)
-    deleted_at = Column(DateTime, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    update_at = Column(DateTime, nullable=True)
+    deleted_at = Column(DateTime, nullable=True)
