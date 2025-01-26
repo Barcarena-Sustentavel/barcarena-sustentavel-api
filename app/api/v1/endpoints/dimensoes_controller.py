ce6fd4381e3552ffc9ab6bda7f563d092b03d934
Marrior <mrlimaroberto@gmail.com>
2025-01-26 15:36:01 -0300

    endpoints corrigidos

--- app/api/v1/endpoints/dimensoes_controller.py
+++ app/api/v1/endpoints/dimensoes_controller.py
@@ -9,9 +9,11 @@ from typing import Any
 
 dimensoes = APIRouter()
 
+#Retorna todos os indicadores de uma dimensão
+#dimensaoNome: nome da dimensão
+#session: sessão do banco de dados
 @dimensoes.get("/dimensoes/{dimensaoNome}/")
 async def get_dimensao(dimensaoNome: str, session: Session = Depends(get_db),status_code=HTTPStatus.OK) -> Any:
-    #print(get_model_id(dimensaoNome, session, dimensao.Dimensao))
     indicadores = session.scalars(select(indicador.Indicador).where(
         indicador.Indicador.fkDimensao_id == await get_model_id(dimensaoNome, session, dimensao.Dimensao)
     ))
@@ -24,54 +26,52 @@ async def get_dimensao(dimensaoNome: str, session: Session = Depends(get_db),sta
     indicadoresall = indicadores.all()
     refrenciasall = refrencias.all()
 
-
     for a in indicadoresall:
-        indicadoresDimensao.append(indicador_schema.IndicadorSchema(id=a.id, fkDimensao_id=a.fkDimensao_id, nome=a.nome))
+        indicadoresDimensao.append(indicador_schema.IndicadorSchema(id=a.id, fkDimensao=a.fkDimensao_id, nome=a.nome))
     for b in refrenciasall:
-        referenciasIndicador.append(referencia_schema.ReferenciaSchema(id=b.id, nome=b.nome, fkDimensao_id=b.fkDimensao_id))
+        referenciasIndicador.append(referencia_schema.ReferenciaSchema(id=b.id, nome=b.nome, fkDimensao=b.fkDimensao_id, link=b.link))
     
     return {"indicadores":indicadoresDimensao, "referencias":referenciasIndicador}
 
 @dimensoes.get("/dimensoes/kml/{dimensaoNome}/")
 async def get_kml(dimensaoNome: str, session: Session = Depends(get_db),status_code=HTTPStatus.OK):
     kmls = session.scalars(select(kml.KML).where(
-          kml.KML.fkDimensao_id == get_model_id(dimensaoNome, session, dimensao.Dimensao)
+          kml.KML.fkDimensao_id == await get_model_id(dimensaoNome, session, dimensao.Dimensao)
          )
     )
     kmls_list = []
 
     for k in kmls.all():
-        kmls_list.append(kml_schema.KMLSchema(id=k.id, name=k.name, fkDimensao_id=k.fkDimensao_id))
+        kmls_list.append(kml_schema.KMLSchema(id=k.id, nome=k.name, fkDimensao=k.fkDimensao_id))
     
     return {"kmls":kmls_list}
 
 @dimensoes.get("/dimensoes/kmlCoords/{kmlNome}/")
 async def get_kml_coords(kmlNome: str, session: Session = Depends(get_db), status_code=HTTPStatus.OK):
     anexos = session.scalars(select(anexo.Anexo).where(
-        anexo.Anexo.fkKML_id == get_model_id(kmlNome, session, kml.KML)
+        anexo.Anexo.fkKML_id == await get_model_id(kmlNome, session, kml.KML)
     ))
-    anexoJson = anexo_schema.AnexoSchema(id=anexos.all()[0].id, fkIndicador_id=anexos.all()[0].fkIndicador_id, fkKML_id=anexos.all()[0].fkKML_id, path=anexos.all()[0].path)
+    anexoJson = anexo_schema.AnexoSchema(id=anexos.all()[0].id, fkIndicador=anexos.all()[0].fkIndicador_id, fkKML=anexos.all()[0].fkKML_id, path=anexos.all()[0].path)
     return {"cordenadas":anexoJson.path}
 
 
 @dimensoes.get("/dimensoes/{dimensaoNome}/{indicadorNome}/")
 async def get_indicador(dimensaoNome: str, indicadorNome: str, session: Session = Depends(get_db), status_code=HTTPStatus.OK):
     indicadorDimensao = session.scalar(select(indicador.Indicador).where(
-        indicador.Indicador.nome == indicadorNome and indicador.Indicador.fkDimensao_id == get_model_id(dimensaoNome, session, dimensao.Dimensao)
+        indicador.Indicador.nome == indicadorNome and indicador.Indicador.fkDimensao_id == await get_model_id(dimensaoNome, session, dimensao.Dimensao)
     ))
     anexoIndicador = session.scalar(select(anexo.Anexo).where(
-        anexo.Anexo.fkIndicador_id == get_model_id(indicadorNome, session, indicador.Indicador)
+        anexo.Anexo.fkIndicador_id == await get_model_id(indicadorNome, session, indicador.Indicador)
     ))
 
-    indicadorDimensaoJson = indicador_schema.IndicadorSchema(id=indicadorDimensao.id, nome=indicadorDimensao.nome, fkDimensao_id=indicadorDimensao.fkDimensao_id)
-    anexoIndicadorJson = anexo_schema.AnexoSchema(id=anexoIndicador.id, fkIndicador_id=anexoIndicador.fkIndicador_id, fkKML_id=anexoIndicador.fkKML_id, path=anexoIndicador.path)
+    indicadorDimensaoJson = indicador_schema.IndicadorSchema(id=indicadorDimensao.id, nome=indicadorDimensao.nome, fkDimensao=indicadorDimensao.fkDimensao_id)
+    anexoIndicadorJson = anexo_schema.AnexoSchema(id=anexoIndicador.id, fkIndicador=anexoIndicador.fkIndicador_id, fkKml=anexoIndicador.fkKML_id, path=anexoIndicador.path)
 
     return {"indicador":indicadorDimensaoJson, "anexo":anexoIndicadorJson}
 
 @dimensoes.post("/contribuicao/")
 async def post_contribuicao(contribuicao: contribuicao_schema.ContribuicaoSchema, status_code=HTTPStatus.CREATED):
     return contribuicao
-
 
 #from fastapi import APIRouter
 #
