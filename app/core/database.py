defd2f9dfc7d18981f6cf44c6c0d6e5959c285d6
Marrior-dot <mrlimaroberto@gmail.com>
2025-08-07 12:31:24 -0300

    branch de produção

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-#SQLALCHEMY_DATABASE_URL = "postgresql://barcarena_sustentavel:barcarenasustentavel@localhost:5432/barcarena_sustentavel"  # Replace with your PostgreSQL connection string
 SQLALCHEMY_DATABASE_URL = "postgresql://barcarena_sustentavel:barcarenasustentavel@barcarena-postgresql:5432/barcarena_sustentavel"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True) # pool_pre_ping for robust connections
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
