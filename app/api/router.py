7948223626156eea61c1686736b748e366e660e8
Marrior-dot <mrlimaroberto@gmail.com>
2026-01-28 09:53:17 -0300

    modificado o database.py para identificar se o ambiente é de produção ou desenvolvimento, adicionado o estudos_complementares.py no router.py

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import dimensoes_controller, kml_controller, contribuicao_controller, indicadores_controller,anexo_controller, referencias_controller,user_controller
+from app.api.v1.endpoints import dimensoes_controller, kml_controller, contribuicao_controller, indicadores_controller,anexo_controller, referencias_controller,user_controller, estudo_complementar_controller
 
 
 router = APIRouter()
@@ -11,3 +11,4 @@ router.include_router(indicadores_controller.indicadorRouter)
 router.include_router(anexo_controller.anexoRouter)
 router.include_router(referencias_controller.referenciasRouter)
 router.include_router(user_controller.userRouter)
+router.include_router(estudo_complementar_controller.estudoComplementarRouter)
--- app/core/database.py
+++ app/core/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 node_env = os.environ.get('NODE_ENV')
-
-SQLALCHEMY_DATABASE_URL = "postgresql://barcarena_sustentavel:barcarenasustentavel@54.233.210.68:6000/barcarena_sustentavel"
+database_url = "postgresql://barcarena_sustentavel:barcarenasustentavel@54.233.210.68:6000/barcarena_sustentavel"
+if node_env == 'production':
+    database_url ="postgresql://barcarena_sustentavel:barcarenasustentavel@barcarena-postgresql:5432/barcarena_sustentavel"
+SQLALCHEMY_DATABASE_URL = database_url
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True) # pool_pre_ping for robust connections
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
