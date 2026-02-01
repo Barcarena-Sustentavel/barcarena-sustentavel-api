0ff4e4c4765769a656b4e8607068464421711324
Marrior-dot <mrlimaroberto@gmail.com>
2026-02-01 11:25:03 -0300

    controle das referencias modificado

--- app/api/v1/endpoints/referencias_controller.py
+++ app/api/v1/endpoints/referencias_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends, HTTPException
+from fastapi import APIRouter,Depends, HTTPException, exceptions
 from app.domain.schemas import referencia_schema 
 from app.domain.models import dimensao, referencias
 from app.domain.schemas.response_schemas.get_dimensao_response import DimensaoData
@@ -6,25 +6,43 @@ from http import HTTPStatus
 from app.core.database import get_db
 from sqlalchemy.orm import Session
 from sqlalchemy import select
-from typing import Any, List
+from typing import Any, List, Optional, Union
 from .aux.get_model_id import get_model_id
 
 referenciasRouter = APIRouter()
 
-@referenciasRouter.get("/admin/dimensoes/{dimensaoNome}/referencias/",response_model=List[str],status_code=HTTPStatus.OK)
-async def get_admin_referencias(dimensaoNome: str, session: Session = Depends(get_db),status_code=HTTPStatus.OK) -> List[str]:
+#@referenciasRouter.get("/admin/dimensoes/{dimensaoNome}/referencias/",response_model=List[str],status_code=HTTPStatus.OK)
+@referenciasRouter.get("/admin/dimensoes/{dimensaoNome}/referencias/",status_code=HTTPStatus.OK)
+#async def get_admin_referencias(dimensaoNome: str, session: Session = Depends(get_db),status_code=HTTPStatus.OK) -> List[str]:
+async def get_admin_referencias(dimensaoNome: str,referenciaNome: Optional[str] = None, session: Session = Depends(get_db),status_code=HTTPStatus.OK) -> Any:
     dimensao_id = await get_model_id(dimensaoNome, session, dimensao.Dimensao)
-    referencias_bd = session.scalars(select(referencias.Referencias).where(referencias.Referencias.fkDimensao_id == dimensao_id))
-    referencias_list = []
-    for r in referencias_bd.all():
-        referencias_list.append(r.nome)
-        
-    return {"referencias":referencias_list}
+    if referenciaNome == None:
+        referencias_bd = session.scalars(select(referencias.Referencias).where(referencias.Referencias.fkDimensao_id == dimensao_id))
+        referencias_list = []
+        for r in referencias_bd.all():
+            referencias_list.append(r.nome)
+
+        return {"referencias":referencias_list}
+    try: 
+        #dimensao_id = await get_model_id(dimensaoNome, session, dimensao.Dimensao)
+        print('referencia',referenciaNome)
+        referencia_data = session.scalar(select(referencias.Referencias).where(
+            referencias.Referencias.nome == referenciaNome,
+            referencias.Referencias.fkDimensao_id == dimensao_id
+        ))
+
+        if not referencia_data:
+            raise HTTPException(status_code=404, detail="Referência não encontrada")
+
+        referencia_response = referencia_schema.ReferenciaSchema(nome=referencia_data.nome, link=referencia_data.link)
+
+        return referencia_response
+    except Exception as e:
+        print('Error',e)
 
 @referenciasRouter.post("/admin/dimensoes/{dimensaoNome}/referencias/", response_model=referencia_schema.ReferenciaSchema,status_code=HTTPStatus.CREATED)
 async def post_admin_referencia(dimensaoNome: str, referenciaNova: referencia_schema.ReferenciaSchema, session: Session = Depends(get_db),status_code=HTTPStatus.CREATED) -> Any:
     dimensao_id = await get_model_id(dimensaoNome, session, dimensao.Dimensao)
-
     new_referencia = referencias.Referencias(nome=referenciaNova.nome, link=referenciaNova.link, fkDimensao_id=dimensao_id)
     session.add(new_referencia)
     session.commit()
@@ -33,22 +51,6 @@ async def post_admin_referencia(dimensaoNome: str, referenciaNova: referencia_sc
     referencia_response = referencia_schema.ReferenciaSchema(nome=new_referencia.nome, link=new_referencia.link)
 
     return referencia_response
-
-@referenciasRouter.get("/admin/dimensoes/{dimensaoNome}/referencias/{referenciaNome}/", response_model=referencia_schema.ReferenciaSchema, status_code=HTTPStatus.OK)
-async def get_admin_referencia_detail(dimensaoNome: str, referenciaNome: str, session: Session = Depends(get_db), status_code=HTTPStatus.OK) -> Any:
-    dimensao_id = await get_model_id(dimensaoNome, session, dimensao.Dimensao)
-    referencia_data = session.scalar(select(referencias.Referencias).where(
-        referencias.Referencias.nome == referenciaNome,
-        referencias.Referencias.fkDimensao_id == dimensao_id
-    ))
-    
-    if not referencia_data:
-        raise HTTPException(status_code=404, detail="Referência não encontrada")
-    
-    referencia_response = referencia_schema.ReferenciaSchema(nome=referencia_data.nome, link=referencia_data.link)
-    
-    return referencia_response
-
 
 @referenciasRouter.patch("/admin/dimensoes/{dimensaoNome}/referencias/{referenciaNome}/", response_model=referencia_schema.ReferenciaSchema,status_code=HTTPStatus.OK)
 async def patch_admin_referencia(dimensaoNome: str, referenciaNome: str, referenciaModificado: referencia_schema.ReferenciaSchema, session: Session = Depends(get_db),status_code=HTTPStatus.OK) -> Any:
